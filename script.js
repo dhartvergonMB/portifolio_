b3638d520691e7554c3cfc81297c71ce3048672e
matheus <matheusbar750@gmail.com>
2022-02-22 02:07:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector(".hamburguer").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
+function enviar(){
+    alert("Entraremos em contato assim que possivel!")
+}
+function tela(){
+    alert("JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.")
+}
